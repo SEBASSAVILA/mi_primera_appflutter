68e49cc4fc5acfc0805b65f3e092e914e5010961
SEBASSAVILA <bobasska8@gmail.com>
2026-08-19 21:03:55 -0500

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Mi primera pagina en Flutter'),
+      home: const MyHomePage(title: 'Flutter'),
     );
   }
 }
